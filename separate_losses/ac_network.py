a5ab9757eecd009c581fe80ebe588f5d25599fd7
Isaac <IsachenkoA@gmail.com>
2017-10-26 14:36:35 +0200

    separate_losses moved in to the folder. They doesn'work well

---
+++ separate_losses/ac_network.py
@@ -0,0 +1,86 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from utils import utils
+
+
+class AC_Network():
+    def __init__(self, s_size, a_size, scope, trainer):
+        with tf.variable_scope(scope):
+            # Input and visual encoding layers
+            self.inputs = tf.placeholder(shape=[None, s_size], dtype=tf.float32)
+            self.imageIn = tf.reshape(self.inputs, shape=[-1, 84, 84, 1])
+            self.conv1 = slim.conv2d(activation_fn=tf.nn.elu,
+                                     inputs=self.imageIn, num_outputs=16,
+                                     kernel_size=[8, 8], stride=[4, 4], padding='VALID')
+            self.conv2 = slim.conv2d(activation_fn=tf.nn.elu,
+                                     inputs=self.conv1, num_outputs=32,
+                                     kernel_size=[4, 4], stride=[2, 2], padding='VALID')
+            hidden = slim.fully_connected(slim.flatten(self.conv2), 256, activation_fn=tf.nn.elu)
+
+            # Recurrent network for temporal dependencies
+            lstm_cell = tf.contrib.rnn.BasicLSTMCell(256, state_is_tuple=True)
+            c_init = np.zeros((1, lstm_cell.state_size.c), np.float32)
+            h_init = np.zeros((1, lstm_cell.state_size.h), np.float32)
+            self.state_init = [c_init, h_init]
+            #print("stat inti", self.state_init)
+            #print("lstm state size", lstm_cell.state_size.c, lstm_cell.state_size.h)
+            c_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.c])
+            h_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.h])
+            self.state_in = (c_in, h_in)
+            rnn_in = tf.expand_dims(hidden, [0])
+            step_size = tf.shape(self.imageIn)[:1]
+            #print("step", step_size)
+            #print("shape", tf.shape(self.imageIn))
+            state_in = tf.contrib.rnn.LSTMStateTuple(c_in, h_in)
+            lstm_outputs, lstm_state = tf.nn.dynamic_rnn(
+                lstm_cell, rnn_in, initial_state=state_in, sequence_length=step_size,
+                time_major=False)
+            lstm_c, lstm_h = lstm_state
+            self.state_out = (lstm_c[:1, :], lstm_h[:1, :])
+            rnn_out = tf.reshape(lstm_outputs, [-1, 256])
+
+            # Output layers for policy and value estimations
+            self.policy = slim.fully_connected(rnn_out, a_size,
+                                               activation_fn=tf.nn.softmax,
+                                               weights_initializer=utils.normalized_columns_initializer(0.01),
+                                               biases_initializer=None)
+            self.value = slim.fully_connected(rnn_out, 1,
+                                              activation_fn=None,
+                                              weights_initializer=utils.normalized_columns_initializer(1.0),
+                                              biases_initializer=None)
+
+            # Only the worker network need ops for loss functions and gradient updating.
+            if scope != 'global':
+                self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
+                self.actions_onehot = tf.one_hot(self.actions, a_size, dtype=tf.float32)
+                self.target_v = tf.placeholder(shape=[None], dtype=tf.float32)
+                self.advantages = tf.placeholder(shape=[None], dtype=tf.float32)
+                #self.advantages = self.target_v - tf.reshape(self.value, [-1])
+
+                self.responsible_outputs = tf.reduce_sum(self.policy * self.actions_onehot, [1])
+
+                # Loss functions
+                self.value_loss = 0.5 * tf.reduce_sum(tf.square(self.target_v - tf.reshape(self.value, [-1])))
+                self.entropy = - tf.reduce_sum(self.policy * tf.log(self.policy))
+                self.policy_loss = -tf.reduce_sum(tf.log(self.responsible_outputs) * self.advantages) - self.entropy * 0.01
+                #self.loss = 0.5 * self.value_loss + self.policy_loss - self.entropy * 0.01
+
+                # Get gradients from local network using local losses
+                local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
+
+                #VALUE grads
+                self.gradients_value = tf.gradients(self.value_loss, local_vars)
+                self.var_norms_value = tf.global_norm(local_vars)
+                grads_value, self.grad_norms_value = tf.clip_by_global_norm(self.gradients_value, 40.0)
+
+                #POLICY grads
+                self.gradients_policy = tf.gradients(self.policy_loss, local_vars)
+                self.var_norms_policy = tf.global_norm(local_vars)
+                grads_policy, self.grad_norms_policy = tf.clip_by_global_norm(self.gradients_policy, 40.0)
+
+                # Apply local gradients to global network
+                global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
+                self.apply_grads_value = trainer.apply_gradients(zip(grads_value, global_vars))
+                self.apply_grads_policy = trainer.apply_gradients(zip(grads_policy, global_vars))
+
---
+++ separate_losses/worker.py
@@ -0,0 +1,213 @@
+from ac_network import AC_Network
+from utils.helper import *
+from utils.utils import *
+from config import *
+import scipy.misc
+
+
+class Worker():
+    def __init__(self, game, name, s_size, a_size, trainer, model_path, global_episodes):
+        self.name = "worker_" + str(name)
+        self.number = name
+        self.model_path = model_path
+        self.trainer = trainer
+        self.global_episodes = global_episodes
+        self.increment = self.global_episodes.assign_add(1)
+        self.episode_rewards = []
+        self.episode_lengths = []
+        self.episode_mean_values = []
+        self.summary_writer = tf.summary.FileWriter(statistics_path + str(self.number))
+
+        # Create the local copy of the network and the tensorflow op to copy global paramters to local network
+        self.local_AC = AC_Network(s_size, a_size, self.name, trainer)
+        self.update_local_ops = update_target_graph('global', self.name)
+
+        # The Below code is related to setting up the Doom environment
+        # game.set_doom_scenario_path("basic.wad")  # This corresponds to the simple task we will pose our agent
+        # game.set_doom_map("map01")
+        # game.set_screen_resolution(ScreenResolution.RES_160X120)
+        # game.set_screen_format(ScreenFormat.GRAY8)
+        # game.set_render_hud(False)
+        # game.set_render_crosshair(False)
+        # game.set_render_weapon(True)
+        # game.set_render_decals(False)
+        # game.set_render_particles(False)
+        # game.add_available_button(Button.MOVE_LEFT)
+        # game.add_available_button(Button.MOVE_RIGHT)
+        # game.add_available_button(Button.ATTACK)
+        # game.add_available_game_variable(GameVariable.AMMO2)
+        # game.add_available_game_variable(GameVariable.POSITION_X)
+        # game.add_available_game_variable(GameVariable.POSITION_Y)
+        # game.set_episode_timeout(300)
+        # game.set_episode_start_time(10)
+        # game.set_window_visible(False)
+        # game.set_sound_enabled(False)
+        # game.set_living_reward(-1)
+        # game.set_mode(Mode.PLAYER)
+        # game.init()
+        self.actions = self.actions = np.identity(a_size, dtype=bool).tolist()
+        self.env = game
+
+    def train(self, rollout, sess, gamma, bootstrap_value):
+        rollout = np.array(rollout)
+        observations = rollout[:, 0]
+        actions = rollout[:, 1]
+        rewards = rollout[:, 2]
+        next_observations = rollout[:, 3]
+        values = rollout[:, 5]
+
+        # Here we take the rewards and values from the rollout, and use them to
+        # generate the advantage and discounted returns.
+        # The advantage function uses "Generalized Advantage Estimation"
+        self.rewards_plus = np.asarray(rewards.tolist() + [bootstrap_value])
+        discounted_rewards = discount(self.rewards_plus, gamma)[:-1]
+        self.value_plus = np.asarray(values.tolist() + [bootstrap_value])
+        advantages = rewards + gamma * self.value_plus[1:] - self.value_plus[:-1]
+        #advantages = discounted_rewards + gamma * self.value_plus[1:] - self.value_plus[:-1]
+        #advantages = discount(advantages, gamma)
+
+        # Update the global network using gradients from loss
+        # Generate network statistics to periodically save
+        feed_dict = {self.local_AC.target_v: discounted_rewards,
+                     self.local_AC.inputs: np.vstack(observations),
+                     self.local_AC.actions: actions,
+                     self.local_AC.advantages: advantages,
+                     self.local_AC.state_in[0]: self.batch_rnn_state[0],
+                     self.local_AC.state_in[1]: self.batch_rnn_state[1]}
+        v_l, p_l, e_l, g_n_v, g_n_p, v_n_v, v_n_p, self.batch_rnn_state, _, _ = sess.run([self.local_AC.value_loss,
+                                                                     self.local_AC.policy_loss,
+                                                                     self.local_AC.entropy,
+                                                                     self.local_AC.grad_norms_value,
+                                                                     self.local_AC.grad_norms_policy,
+                                                                     self.local_AC.var_norms_value,
+                                                                     self.local_AC.var_norms_policy,
+                                                                     self.local_AC.state_out,
+                                                                     self.local_AC.apply_grads_value,
+                                                                     self.local_AC.apply_grads_policy],
+                                                                    feed_dict=feed_dict)
+        return v_l / len(rollout), p_l / len(rollout), e_l / len(rollout), g_n_v, g_n_p, v_n_v, v_n_p
+
+    def work(self, max_episode_length, gamma, sess, coord, saver):
+        episode_count = sess.run(self.global_episodes)
+        total_steps = 0
+        print("Starting worker " + str(self.number))
+        with sess.as_default(), sess.graph.as_default(), coord.stop_on_exception():
+            while not coord.should_stop():
+                sess.run(self.update_local_ops)
+                episode_buffer = []
+                episode_values = []
+                episode_frames = []
+                episode_reward = 0
+                episode_step_count = 0
+                d = False
+
+                self.env.new_episode()
+
+                s = self.env.get_state().image
+                s = process_frame(s)
+
+
+                rnn_state = self.local_AC.state_init
+                self.batch_rnn_state = rnn_state
+                while self.env.is_episode_finished() == False:
+                    # Take an action using probabilities from policy network output.
+                    a_dist, v, rnn_state = sess.run(
+                        [self.local_AC.policy, self.local_AC.value, self.local_AC.state_out],
+                        feed_dict={self.local_AC.inputs: [s],
+                                   self.local_AC.state_in[0]: rnn_state[0],
+                                   self.local_AC.state_in[1]: rnn_state[1]})
+                    a = np.random.choice(a_dist[0], p=a_dist[0])
+                    a = np.argmax(a_dist == a)
+                    self.env.make_action(self.actions[a])
+                    r = self.env.get_reward()
+                    d = self.env.is_episode_finished()
+                    if d == False:
+                        s1 = self.env.get_state().image
+                        if self.number == 0:
+                            side_s = self.env.get_state().side_image
+                            frame = process_gif(s1, side_s)
+                            episode_frames.append(frame)
+
+                        s1 = process_frame(s1)
+
+                    else:
+                        s1 = s
+
+                    episode_buffer.append([s, a, r, s1, d, v[0, 0]])
+                    episode_values.append(v[0, 0])
+
+                    episode_reward += r
+                    s = s1
+                    total_steps += 1
+                    episode_step_count += 1
+
+                    # If the episode hasn't ended, but the experience buffer is full, then we
+                    # make an update step using that experience rollout.
+                    if len(episode_buffer) == 30 and d != True and episode_step_count != max_episode_length - 1:
+                        # Since we don't know what the true final return is, we "bootstrap" from our current
+                        # value estimation.
+                        v1 = sess.run(self.local_AC.value,
+                                      feed_dict={self.local_AC.inputs: [s],
+                                                 self.local_AC.state_in[0]: rnn_state[0],
+                                                 self.local_AC.state_in[1]: rnn_state[1]})[0, 0]
+                        v_l, p_l, e_l, g_n_v, g_n_p, v_n_v, v_n_p = self.train(episode_buffer, sess, gamma, v1)
+                        episode_buffer = []
+                        sess.run(self.update_local_ops)
+                    if d == True:
+                        break
+
+                self.episode_rewards.append(episode_reward)
+                self.episode_lengths.append(episode_step_count)
+                self.episode_mean_values.append(np.mean(episode_values))
+
+                # Update the network using the episode buffer at the end of the episode.
+                if len(episode_buffer) != 0:
+                    v_l, p_l, e_l, g_n_v, g_n_p, v_n_v, v_n_p = self.train(episode_buffer, sess, gamma, 0.0)
+
+                # Periodically save gifs of episodes, model parameters, and summary statistics.
+                #print("Episode count:",episode_count)
+                if episode_count % STATISTICS_SAVE_TIME_STEP == 0 and episode_count != 0:
+                    if self.number == 0 and episode_count % IMAGE_SAVE_TIME_STEP == 0:
+                        time_per_step = 0.05
+                        images = np.array(episode_frames)
+                        make_gif(images, frames_path + '/image' + str(episode_count) + '.gif',
+                                 duration=len(images) * time_per_step, true_image=True, salience=False)
+
+                        #scipy.misc.toimage(images[10], cmin=0.0, cmax=...).save(frames_path + '/image' + str(episode_count) + '.jpg')
+                        #scipy.misc.imsave(frames_path + '/image' + str(episode_count) + '.jpg', images[10])
+
+                    if episode_count % MODEL_SAVE_TIME_STEP == 0 and self.number == 0:
+                        saver.save(sess, self.model_path + '/model-' + str(episode_count) + '.cptk')
+                        print("Saved Model")
+
+
+                    mean_reward = np.mean(self.episode_rewards[-5:])
+                    mean_length = np.mean(self.episode_lengths[-5:])
+                    mean_value = np.mean(self.episode_mean_values[-5:])
+                    summary = tf.Summary()
+                    summary.value.add(tag='Perf/Reward', simple_value=float(mean_reward))
+                    summary.value.add(tag='Perf/Length', simple_value=float(mean_length))
+                    summary.value.add(tag='Perf/Value', simple_value=float(mean_value))
+                    summary.value.add(tag='Losses/Value Loss', simple_value=float(v_l))
+                    summary.value.add(tag='Losses/Policy Loss', simple_value=float(p_l))
+                    summary.value.add(tag='Losses/Entropy', simple_value=float(e_l))
+                    summary.value.add(tag='Losses/Grad Norm value', simple_value=float(g_n_v))
+                    summary.value.add(tag='Losses/Grad Norm policy', simple_value=float(g_n_p))
+                    summary.value.add(tag='Losses/Var Norm value', simple_value=float(v_n_v))
+                    summary.value.add(tag='Losses/Var Norm policy', simple_value=float(v_n_p))
+                    self.summary_writer.add_summary(summary, episode_count)
+
+                    self.summary_writer.flush()
+
+                    print(self.name, ": episode: ", episode_count, "mean reward: ", mean_reward)
+                if self.number == 0:
+                    sess.run(self.increment)
+                episode_count += 1
+
+                if episode_count == MAX_NUMBER_OF_EPISODES:
+                    self.env.__del__()
+                    if self.number == 0:
+                        coord.request_stop()
+                    else:
+                        coord.wait_for_stop()
+                    print(self.name, ": work finished")
