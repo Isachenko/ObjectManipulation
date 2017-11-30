537607470fea53086882cd76d812734a4be87358
Khmer <adrianalvarez15@gmail.com>
2017-11-30 12:41:35 +0100

    Network with gaussian implementation for continuous version

---
+++ ac_network_continuous_gaussian.py
@@ -0,0 +1,97 @@
+ximport numpy as np
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from utils import utils
+
+
+class ACNetworkContinuous():
+    def __init__(self, s_size, a_size, scope, trainer):
+        A_BOUND_LOW = -1
+        A_BOUND_HIGH = 1
+
+        with tf.variable_scope(scope):
+            # Input and visual encoding layers
+            self.inputs = tf.placeholder(shape=[None, s_size], dtype=tf.float32)
+            self.imageIn = tf.reshape(self.inputs, shape=[-1, 84, 84, 1])
+            self.conv1 = slim.conv2d(activation_fn=tf.nn.elu,
+                                     inputs=self.imageIn, num_outputs=16,
+                                     kernel_size=[8, 8], stride=[4, 4], padding='VALID')
+            self.conv2 = slim.conv2d(activation_fn=tf.nn.elu,
+                                     inputs=self.conv1, num_outputs=32,
+                                     kernel_size=[4, 4], stride=[2, 2], padding='VALID')
+            hidden = slim.fully_connected(slim.flatten(self.conv2), 256, activation_fn=tf.nn.elu)
+
+            # Recurrent network for temporal dependencies
+            lstm_cell = tf.contrib.rnn.BasicLSTMCell(256, state_is_tuple=True)
+            c_init = np.zeros((1, lstm_cell.state_size.c), np.float32)
+            h_init = np.zeros((1, lstm_cell.state_size.h), np.float32)
+            self.state_init = [c_init, h_init]
+            c_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.c])
+            h_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.h])
+            self.state_in = (c_in, h_in)
+            rnn_in = tf.expand_dims(hidden, [0])
+            step_size = tf.shape(self.imageIn)[:1]
+            state_in = tf.contrib.rnn.LSTMStateTuple(c_in, h_in)
+            lstm_outputs, lstm_state = tf.nn.dynamic_rnn(
+                lstm_cell, rnn_in, initial_state=state_in, sequence_length=step_size,
+                time_major=False)
+            lstm_c, lstm_h = lstm_state
+            self.state_out = (lstm_c[:1, :], lstm_h[:1, :])
+            rnn_out = tf.reshape(lstm_outputs, [-1, 256])
+
+            # Output layers for policy and value estimations
+            self.policy_mean = slim.fully_connected(rnn_out, a_size,
+                                               activation_fn=tf.nn.tanh,
+                                               weights_initializer=utils.normalized_columns_initializer(0.01),
+                                               biases_initializer=None)
+            self.policy_sigma = slim.fully_connected(rnn_out, a_size,
+                                               activation_fn=tf.nn.tanh,
+                                               weights_initializer=utils.normalized_columns_initializer(0.01),
+                                               biases_initializer=None)
+            self.value = slim.fully_connected(rnn_out, 1,
+                                              activation_fn=None,
+                                              weights_initializer=utils.normalized_columns_initializer(1.0),
+                                              biases_initializer=None)
+
+            self.A = tf.clip_by_value(tf.squeeze(normal_dist.sample(1), axis=0), A_BOUND_LOW , A_BOUND_HIGH)
+
+            # Only the worker network need ops for loss functions and gradient updating.
+            if scope != 'global':
+                self.actions = tf.placeholder(shape=[None, 2*a_size], dtype=tf.float32)
+                self.target_v = tf.placeholder(shape=[None], dtype=tf.float32)
+                self.advantages = tf.placeholder(shape=[None], dtype=tf.float32)
+                self.print_adv = tf.Print(self.advantages, [self.advantages])
+                self.actions_reshaped = tf.reshape(self.actions, shape=[-1, 2*a_size])
+                self.actions_diff = tf.reduce_sum(tf.square(self.actions_reshaped - self.policy), [1]) #think more
+
+
+                mu, sigma = self.policy_mean * A_BOUND_HIGH, self.policy_sigma + 1e-4
+
+                normal_dist = tf.distributions.Normal(mu, sigma)
+
+
+
+                # Loss functions
+                #self.value_loss = 0.5 * tf.reduce_sum(tf.square(self.target_v - tf.reshape(self.value, [-1])))
+                # self.c_loss = tf.reduce_mean(tf.square(td))
+
+                log_prob = normal_dist.log_prob(self.a_his)
+                exp_v = log_prob * td
+                entropy = normal_dist.entropy()  # encourage exploration
+                self.exp_v = ENTROPY_BETA * entropy + exp_v
+                self.a_loss = tf.reduce_mean(-self.exp_v)
+
+                #self.entropy = - tf.reduce_sum(self.policy * tf.log(self.policy))
+                #self.policy_loss = 0.5 * tf.reduce_sum(self.actions_diff * self.advantages)
+
+                #self.loss = 0.5 * self.value_loss + self.policy_loss - self.entropy * 0.01# + self.print_adv
+
+                # Get gradients from local network using local losses
+                local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
+                self.gradients = tf.gradients(self.loss, local_vars)
+                self.var_norms = tf.global_norm(local_vars)
+                grads, self.grad_norms = tf.clip_by_global_norm(self.gradients, 40.0)
+
+                # Apply local gradients to global network
+                global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
+                self.apply_grads = trainer.apply_gradients(zip(grads, global_vars))
