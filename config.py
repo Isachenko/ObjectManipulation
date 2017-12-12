4631319101da8ae29edecd93b8d999f9f43ab504
Khmer <adrianalvarez15@gmail.com>
2017-12-12 19:38:43 +0100

    Folder has number of workers

--- config.py
+++ config.py
@@ -25,6 +25,7 @@ experiment = {  0 : "speed",
 #vrep related params
 VREP_EXE_PATH = '/Users/Isaac/V-REP_PRO_EDU_V3_4_0_Mac/vrep.app/Contents/MacOS/wrong'
 vf= 0.5
+num_workers = 1
 if len(sys.argv) > 1:
     s = int(sys.argv[2])
     SCENE_PATH = scenes[s]
@@ -32,7 +33,9 @@ if len(sys.argv) > 1:
         RANDOM = True
     else:
         RANDOM = False
-    vf = float(sys.argv[3])
+    num_workers = int(sys.argv[3])
+    vf = float(sys.argv[4])
+
 
 else:
     SCENE_PATH = '/scenes/uarm_gripper.ttt'
@@ -47,18 +50,18 @@ load_model = False
 MAX_EPISODE_LENGTH = 200
 MAX_NUMBER_OF_EPISODES = 1000
 gamma = .99  # discount rate for advantage estimation and reward discounting
-num_workers = 1
+
 
 
 
 #statistics save params
 now = datetime.datetime.now()
 
-results_path = "./archive/results" + now.strftime("_%d-%m-%Y_%H-%M-%S") +'_vf_'+ str(vf)
+results_path = "./archive/results" + now.strftime("_%d-%m-%Y_%H-%M-%S")+'_workers_'+str(num_workers) +'_vf_'+ str(vf)
 if len(sys.argv) > 1:
     var1 = int(sys.argv[1])
     var2 = int(sys.argv[2])
-    results_path = "./archive/results" + now.strftime("_%d-%m-%Y_%H-%M-%S_") + discrete_or_continuous[var1] + "_" + experiment[var2]
+    results_path = "./archive/results" + now.strftime("_%d-%m-%Y_%H-%M-%S_") + discrete_or_continuous[var1] + "_" + experiment[var2]+'_workers_'+str(num_workers) +'_vf_'+ str(vf)
 
 model_path = results_path + '/model'
 frames_path = results_path + '/frames'
