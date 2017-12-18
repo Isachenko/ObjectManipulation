919e1981b099a2f227a1bef17945c22b822df18a
A.Alvarez.Fernandez@student.rug.nl <adrianalvarez15@gmail.com>
2017-12-18 13:50:14 +0100

    change run_A3c.sh file

--- run_A3C.sh
+++ run_A3C.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --time=2:00:00 #This is one hour
+#SBATCH --time=1:00:00 #This is one hour
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=adrianalvarez15@gmail.com
 #SBATCH --output=job-%j.log
-#SBATCH --mem=60GB#(This is max available RAM)
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=120GB#(This is max available RAM)
 cd ObjectManipulation
 module load git/2.13.2-foss-2016a
 module load Python/3.5.2-foss-2016a
@@ -12,4 +13,4 @@ module load matplotlib/1.5.3-foss-2016a-Python-3.5.2
 module load libGLU/9.0.0-foss-2016a-Mesa-11.2.1
 module load V-REP/3.4.0
 module load FFmpeg/3.0.2-foss-2016a
-python3 A3C_experiment.py
+python3 main.py $*
